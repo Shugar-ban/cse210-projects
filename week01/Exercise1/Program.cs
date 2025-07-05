308db4b84b6d4803f81b138b1f97019f98b57ba0
Shugar-ban <adeoyeayodeji913@gmail.com>
2025-07-05 11:11:57 -0400

    first name and last name wk01

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("what is your first name?");
+        Console.WriteLine("What is your last name?");
+
+        string f_name = Console.ReadLine();
+        Console.WriteLine(f_name);
+        string l_name = Console.ReadLine();
+        Console.WriteLine(l_name);
     }
 }
